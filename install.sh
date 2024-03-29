92ccd494038134e717e07c4e0a65f20d85051171
avotcorg <102286982+avotcorg@users.noreply.github.com>
2024-03-29 10:38:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ git clone https://github.com/avotcorg/GFW-BOT.git
 cd GFW-BOT
 echo "安装要求"
 chmod +x requirement.sh
-./requirement.sh
 python3 dos2unix.py
+./requirement.sh
 echo " 现在您可以开始添加您的 api 令牌等参数"
 python3 install.py
 echo "安装完成"
